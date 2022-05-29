c7d7eb5c28b372a9f0bbf7624a9b29cff6e0dbf8
efeldel <man-9k@yandex.ru>
2022-05-29 14:16:32 +0300

    random start

--- src/MovingImage.java
+++ src/MovingImage.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MovingImage {
     public static void main(String[] args) throws Exception{
         int coordX = 0;
-        int coordY = 0;
-        int dx = 0;
-        int dy = 0;
+        int coordY = (int) (Math.random() * 660);
+        int dx = 1;
+        int dy = 1;
         BufferedImage image = null;
 
         JFrame jFrame = new JFrame();
@@ -28,18 +28,14 @@ public class MovingImage {
         JLabel picLabel = new JLabel(new ImageIcon(image.getScaledInstance(100,100, Image.SCALE_FAST)));
         jFrame.add(picLabel).setBounds(0,0,100,100);
         while (true) {
-        if (coordY == 0 && coordX < 685 ) {
-            dx = 1;
-            dy = 0;
-        } else if (coordX == 685 && coordY < 660) {
-            dx = 0;
+        if (coordY == 0) {
             dy = 1;
-        } else if (coordY == 660 && coordX > 0) {
+        } else if (coordX == 685) {
             dx = -1;
-            dy = 0;
-        } else if (coordX == 0 && coordY > 0) {
-            dx = 0;
+        } else if (coordY == 660) {
             dy = -1;
+        } else if (coordX == 0) {
+            dx = 1;
         }
             coordX += dx;
             coordY += dy;
