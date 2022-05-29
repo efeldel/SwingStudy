eb0a9c4a3ceb4d73afb2b8b31ef1e57fce76e398
efeldel <man-9k@yandex.ru>
2022-05-29 19:27:07 +0300

    working sun and earth

--- src/MovingImage.java
+++ src/MovingImage.java
@@ -10,15 +10,13 @@ public class MovingImage {
     public static void main(String[] args) throws Exception {
         int coordX = 0;
         int coordY = 0;
-//        int dx = 1;
-//        int dy = 1;
+        int t = 0;
         BufferedImage sun = null;
         BufferedImage earth = null;
 
         JFrame jFrame = new JFrame();
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setBounds(500, 200, 800, 800);
-        //jFrame.getContentPane().setLayout(null);
         jFrame.getContentPane().setBackground(Color.black);
         jFrame.setVisible(true);
 
@@ -31,25 +29,21 @@ public class MovingImage {
         }
 
         JLabel sunLabel = new JLabel(new ImageIcon(sun.getScaledInstance(200, 200, Image.SCALE_FAST)));
+        JLabel earthLabel = new JLabel(new ImageIcon(earth.getScaledInstance(50, 50, Image.SCALE_FAST)));
         jFrame.add(sunLabel);
-        sunLabel.setBounds(300,300,200,200);
+        sunLabel.setBounds(285, 260,200,200);
+        jFrame.add(earthLabel);
+        earthLabel.setBounds(0, 260,200,200);
 
-//        while (true) {
-//            if (coordY == 0) {
-//                dy = 1;
-//            } else if (coordX == 685) {
-//                dx = -1;
-//            } else if (coordY == 660) {
-//                dy = -1;
-//            } else if (coordX == 0) {
-//                dx = 1;
-//            }
-//            coordX += dx;
-//            coordY += dy;
-//            picLabel.setLocation(coordX, coordY);
-//            jFrame.repaint();
-//            Thread.sleep(5);
-//        }
+        while (true) {
+            if (t == 360) t = 0;
+            coordX = (int) (285 + (285 * Math.cos(t * Math.PI / 180)));
+            coordY = (int) (260 + (285 * Math.sin(t * Math.PI / 180)));
+            earthLabel.setLocation(coordX, coordY);
+            jFrame.repaint();
+            Thread.sleep(50);
+            t++;
+        }
     }
 }
 
