5a4efe0505d8ad54581d0f5afe4673995fa35924
efeldel <man-9k@yandex.ru>
2022-05-30 21:42:52 +0300

    Racing smileys

--- src/MovingImage.java
+++ src/MovingImage.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class MovingImage extends Thread {
     int coordX = 0;
+    int coordY = 0;
     int place = 0;
     int size = 0;
     BufferedImage image = null;
@@ -17,47 +18,52 @@ public class MovingImage extends Thread {
         this.jframe = jframe;
         this.size = size;
         this.image = image;
-        this.coordY = place;
+        this.place = place;
     }
     @Override
     public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         JLabel label = new JLabel(new ImageIcon(image.getScaledInstance(size, size, Image.SCALE_FAST)));
-        while (true) {
-            coordX += 1 + (int)  Math.random()*5;
-            coordY = (int) place * 100;
+        do {
+            coordX += 1 + (int) (Math.random()*5);
+            coordY = (place - 1) * (size + 50);
             jframe.add(label);
             label.setBounds(coordX,coordY,size,size);
             label.setLocation(coordX, coordY);
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(1);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-        }
+            jframe.repaint();
+        } while (coordX + size <= jframe.getWidth() - 15);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         BufferedImage car = null;
 
         JFrame jFrame = new JFrame("Racing cars");
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setBounds(500, 200, 800, 1000);
+        jFrame.setBounds(500, 200, 1000, 800);
         jFrame.getContentPane().setBackground(Color.white);
         jFrame.setVisible(true);
 
 
         try {
-            car = ImageIO.read(new File("smile.png"));
+            car = ImageIO.read(new File("smile.jpg"));
         } catch (IOException ex) {
             System.out.println("File not found.");
         }
-
-        new MovingImage(car, jFrame, 100, 0).start();
-        new MovingImage(car, jFrame, 100, 150).start();
-        new MovingImage(car, jFrame, 100, 300).start();
-        new MovingImage(car, jFrame, 100, 450).start();
-        new MovingImage(car, jFrame, 100, 600).start();
+        new MovingImage(car, jFrame, 100, 1).start();
+        new MovingImage(car, jFrame, 100, 2).start();
+        new MovingImage(car, jFrame, 100, 3).start();
+        new MovingImage(car, jFrame, 100, 4).start();
+        new MovingImage(car, jFrame, 100, 5).start();
 
 
     }
